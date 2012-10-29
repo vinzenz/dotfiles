118cb01c2d3d123957200d60c86c234fd4df3ea3
Vinzenz Feenstra <vfeenstr@redhat.com>
2012-10-29 10:48:28 +0100

    We want urxvt256c for colorscheme support

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -37,7 +37,7 @@ end
 beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "urxvt"
+terminal = "urxvt256c"
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
